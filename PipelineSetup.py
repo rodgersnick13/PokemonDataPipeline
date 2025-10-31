76ba677933cb2c7837de1dbec2d0d7f479d5d116
rodgersnick13 <rodgersnick13@gmail.com>
2025-10-31 10:16:54 -0400

    Update data cleaning file

--- PipelineSetup.py
+++ PipelineSetup.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-# Will grab data from github in production, but I'm on metered internet
+# Will grab data from github in "production", but I'm on metered internet
 #url = 'https://raw.githubusercontent.com/rodgersnick13/PokemonDataPipeline/refs/heads/main/pokemon.csv'
 class pokeData:
     df = pd.DataFrame()
@@ -13,7 +13,14 @@ class pokeData:
     def head(self):
         return self.df.head()
     
-print(pokeData().df.head())
+    def dropTypeComparisons(self):
+        df = self.df.drop(['against_bug', 'against_bug', 'against_dark', 'against_dragon',
+                           'against_electric', 'against_fairy', 'against_fight', 'against_fire',
+                           'against_flying', 'against_ghost', 'against_grass', 'against_ground',
+                           'against_ice', 'against_normal', 'against_poison', 'against_psychic',
+                           'against_rock', 'against_steel', 'against_water'])
+        return df
+
 
 
 #path = 'pokemon.csv'
